c54d9a434109e72b20d5c9ed7329e1540ad9b9e6
Lakei87 <monastyrskyis@gmail.com>
2022-01-03 20:13:09 +0100

    копирует папки и файлы с предыдущей работы

---
+++ js/modal.js
@@ -0,0 +1,31 @@
+// ======Скрипт для функционала модалки
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden');
+  }
+})();
+
+// ========Скрипт для отправке данных
+// (() => {
+//   document
+//     .querySelector('.js-speaker-form')
+//     .addEventListener('submit', e => {
+//       e.preventDefault();
+
+//       new FormData(e.currentTarget).forEach((value, name) =>
+//         console.log('${name}: ${value}'),
+//       );
+
+//       e.currentTarget.removeEventListener();
+//     });
+// }
+// )();
